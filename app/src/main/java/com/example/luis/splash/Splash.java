e49dbda05c406ab3c67b249c1c252ab7afcd9676
Gomez <luis.gomez.catalan@gmail.com>
2017-06-14 00:40:00 -0500

    Agregando segundo commit ningun cambio

---
+++ app/src/main/java/com/example/luis/splash/Splash.java
@@ -0,0 +1,24 @@
+package com.example.luis.splash;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(Splash.this, SegundActividad.class);
+                startActivity(intent);
+                finish();
+            }
+        },500);
+    }
+}
